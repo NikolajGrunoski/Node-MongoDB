c71da9b12820269b36b96c50d8e7288fbf10002c
NikolajGrunoski <nikolaj.grunoski@gmail.com>
2019-12-02 22:02:29 +0100

    9

--- services/auth.js
+++ services/auth.js
@@ -4,18 +4,29 @@ var jwt = require('express-jwt');
 const config = require('../config/index.js');
 const db = require('../db/connection');
 const auth = require('../handlers/auth');
+const path = require('path');
 
 
 db.init(config.getConfig('db'));
 
+
+
 var api = express();
+////////////////////////////////
+//Only for testing////////
+////////////////////////////////
+var pub = path.join(__dirname,"..",'public');
+api.use("/public",express.static(pub));
+////////////////////////////////
+//Only for testing////////
+////////////////////////////////
 api.use(bodyParser.json());
 api.use(jwt(
     { secret: config.getConfig('jwt').key }
 
 )
     .unless(
-        { path: ['/api/v1/register', '/api/v1/login'] }
+        { path: ['/api/v1/register', '/api/v1/login','/public'] }
     )
 );
 
@@ -25,6 +36,14 @@ api.get('/api/v1/renew', auth.renew);
 api.post('/api/v1/send-reset-link', auth.resetLink);
 api.post('/api/v1/reset-password', auth.resetPassword);
 api.post('/api/v1/change-password', auth.changePassword);
+
+api.use(function(err, req, res, next){
+    if(err.name === 'UnauthorizedError'){
+        res.status(401).send({error: 'Invalid token'});
+    }else{
+        next(err);
+    }
+})
 
 api.listen(8081, err => {
     if (err) {
